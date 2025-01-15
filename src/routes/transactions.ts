7322392a73887615a790f34e1da4a47a0169d701
Carlos Augusto <caarlos.frei@gmail.com>
2025-01-15 17:29:28 -0300

    tipagem de declaração do TS para auxiliar o knex nas tabelas

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,15 +1,24 @@
 import { FastifyInstance } from "fastify";
-import knex from "knex";
+import { z } from "zod";
+import { randomUUID } from "crypto";
+import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transactions")
-      .insert({
-        id: crypto.randomUUID(),
-        title: "Transação de teste",
-        amount: 1000,
-      })
-      .returning("*");
-    return transaction;
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send();
   });
 }
