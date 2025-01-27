448f90ee3f2af2ad020dec5cb9351c2c4c301a13
Carlos Augusto <caarlos.frei@gmail.com>
2025-01-27 15:52:32 -0300

    Corrigido a leitura da variável de ambiente PORT

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,13 +6,12 @@ const envSchema = z.object({
   DATABASE_CLIENT: z.enum(['sqlite3', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
-})
+});
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
   console.error('Invalid environment variables:', _env.error.format());
-
   throw new Error('Invalid environment variables');
 }
 
